9cbf3244a3e52498637d1acf7633e921d8566b4a
AnotherPompousDunmer <46799659+AnotherPompousDunmer@users.noreply.github.com>
2023-05-04 11:53:17 +1000

    Fixed performance
    -  moved rasterization into separate thread
    - moved conversion to Texture2D into coroutine so it can be chunked between frames because Texture2D operations have to be performed in the main thread >=(

--- Assets/Prefabs/pdf/PdfNavigator.cs
+++ Assets/Prefabs/pdf/PdfNavigator.cs
@@ -3,6 +3,9 @@ using Apitron.PDF.Rasterizer.Configuration;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using System.Threading;
+using Unity.Collections;
+using Unity.Jobs;
 using UnityEngine;
 
 public class PdfNavigator : MonoBehaviour
@@ -14,15 +17,10 @@ public class PdfNavigator : MonoBehaviour
     string path = "F:/.Downloads/jov-11-5-8.pdf";
     Document doc;
 
-    Texture2D tex;
-
     int currentPage = 0;
 
     void Start()
     {
-        tex = new Texture2D(1, 1);
-        GetComponent<MeshRenderer>().material.SetTexture("_BaseMap", tex);
-
         Open();
     }
 
@@ -39,48 +37,72 @@ public class PdfNavigator : MonoBehaviour
 
     void GoToPage(int p)
 	{
+        //SEMAPHORE NEEDED HERE
+
         p = Mathf.Clamp(p, 0, doc.Pages.Count);
         currentPage = p;
 
         Page page = doc.Pages[p];
 
+        StartCoroutine(nameof(ChangeTexture), page);
+    }
+
+
+    bool changeTextureSemaphore = false;
+    const int MAX_TEXTURE_FILL_PER_FRAME = 100000;
+    IEnumerator ChangeTexture(Page page)
+	{
+        //Stop multiple changes at the same time
+        if (changeTextureSemaphore) yield break;
+        changeTextureSemaphore = true;
+
+        //Calculate resolution
         Vector2 fRes = (MAX_RES * new Vector2((float)page.Width, (float)page.Height) / Mathf.Max((float)page.Width, (float)page.Height));
         Vector2Int res = new Vector2Int((int)fRes.x, (int)fRes.y);
 
-        float one = Time.realtimeSinceStartup;
-        int[] data = page.RenderAsInts(res.x, res.y, renderingSettings);
-        string time = (Time.realtimeSinceStartup - one).ToString();
-        one = Time.realtimeSinceStartup;
-        PdfUnityBridgeUtils.WriteTextureFromIntArray(data, res, ref tex);
-        time += ", " + (Time.realtimeSinceStartup - one).ToString();
-        Debug.Log(time);
-    }
-
-	private void Update()
-	{
-        if (Input.GetKeyDown(KeyCode.D)) NextPage();
-        if (Input.GetKeyDown(KeyCode.A)) LastPage();
-    }
-}
-
-public class PdfUnityBridgeUtils
-{
-
-    public static void WriteTextureFromIntArray(int[] arr, Vector2Int res, ref Texture2D tex)
-    {
-        tex.Reinitialize(res.x, res.y);
-
-        for (int i = 0; i < arr.Length; i ++)
+        //Open a new thread to rasterize the pdf
+        int[] arr = null;
+        Thread t = new Thread(() => { arr = page.RenderAsInts(res.x, res.y, renderingSettings); });
+        t.Start();
+        
+        //wait until the thread finishes
+        while (t.ThreadState == ThreadState.Running || t.ThreadState == ThreadState.WaitSleepJoin)
 		{
-            
+            yield return new WaitForEndOfFrame();
+		}
+
+        if (t.ThreadState != ThreadState.Stopped)
+        {
+            Debug.LogError($"Thread for Changing pdf Page stopped early! Thread state {t.ThreadState}");
+            changeTextureSemaphore = false;
+            yield break;
+        }
+
+        //Make a new texture and fill it based on the rasterized pdf
+        Texture2D tex = new Texture2D(res.x, res.y);
+
+        for (int i = 0; i < arr.Length; i++)
+        {
             tex.SetPixel(i % res.x, res.y - (i / res.x), new Color(
                         1 - ((arr[i] << 8) >> 24), //R
                         1 - ((arr[i] << 16) >> 24), //G
                         1 - ((arr[i] << 24) >> 24) //B
                         ));
-		}
+            if ((i+1)%MAX_TEXTURE_FILL_PER_FRAME == 0) yield return new WaitForEndOfFrame();
+        }
 
         tex.Apply();
+
+        GetComponent<MeshRenderer>().material.SetTexture("_BaseMap", tex);
+
+        //Open the semaphores again
+        changeTextureSemaphore = false;
     }
 
+	private void Update()
+	{
+        //TEMPORARY NAVIGATION CODE
+        if (Input.GetKeyDown(KeyCode.D)) NextPage();
+        if (Input.GetKeyDown(KeyCode.A)) LastPage();
+    }
 }
